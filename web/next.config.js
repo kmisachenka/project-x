e3795bc1434725b72fc97c4cec24b7a100a066f4
Kiryl Misachenka <kmisachenka@gmail.com>
2020-07-23 11:03:16 +0300

    chore(docker): add docker-compose

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,8 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   poweredByHeader: false,
+  publicRuntimeConfig: {
+    apiEndpoint: isProduction ? '/graphql' : 'http://localhost:4000/graphql',
+  },
 };
--- web/src/hocs/with-apollo.ts
+++ web/src/hocs/with-apollo.ts
@@ -1,10 +1,13 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import withApollo from 'next-with-apollo';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      uri: process.env.API_URL ?? 'http://localhost:4000/graphql',
+      uri: publicRuntimeConfig.apiEndpoint,
       cache: new InMemoryCache().restore(initialState || {}),
     })
 );
