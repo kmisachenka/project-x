5592e1656b859c3256b246d88d173cff35cb65ea
Kiryl Misachenka <kmisachenka@gmail.com>
2020-09-07 17:24:21 +0300

    chore(deps): add bundle-analyzer plugin

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,19 @@
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = {
+const defaultConfig = {
   poweredByHeader: false,
   publicRuntimeConfig: {
     apiEndpoint: isProduction ? '/graphql' : 'http://localhost:4000/graphql',
   },
+  experimental: {
+    optimizeFonts: true,
+    optimizeImages: true,
+  },
 };
+
+module.exports = withPlugins([withBundleAnalyzer({})], defaultConfig);
